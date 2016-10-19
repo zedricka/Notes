58050da574711193a9d683cec9c774476a56400d
zedricka <zedrick.ara@gmail.com>
2016-10-19 08:06:05 -1000

    UpdatedNotes10/19/16

---
+++ javascript.js
@@ -0,0 +1,29 @@
+// This is a comment
+
+/* this is a block comment 
+
+Anything between here is a comment!
+
+*/
+
+// this is a string
+var myName = "Zedrick";
+
+// this is a integer
+// CTRL + /
+var myAge = 16;
+
+// boolean type (True and False)
+var booleanExample = true;
+
+//  unassigned variable
+undefined
+
+// not a number
+NaN
+
+// array
+
+// objects
+
+var came1CaseExample = "yay"
